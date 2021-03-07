c1e8cc3b24a95b3d6ebe66ca945d14452f1463ef
James Santucci <james.santucci@gmail.com>
2021-03-07 11:22:18 -0700

    remove dev error message

--- src/pages/index.js
+++ src/pages/index.js
@@ -15,9 +15,7 @@ const BlogIndex = ({ data, location }) => {
         <SEO title="All posts" />
         <Bio />
         <p>
-          No blog posts found. Add markdown posts to "content/blog" (or the
-          directory you specified for the "gatsby-source-filesystem" plugin in
-          gatsby-config.js).
+          No blog posts found.
         </p>
       </Layout>
     )
