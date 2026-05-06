f2105d102f4c2296e3347c310bc3105e07e3064a
James Santucci <james.santucci@gmail.com>
2026-05-05 17:10:47 -0700

    Add robots.txt

--- site.hs
+++ site.hs
@@ -7,9 +7,13 @@ import Hakyll
 --------------------------------------------------------------------------------
 main :: IO ()
 main = hakyll $ do
-  match "images/*" $ do
-    route idRoute
-    compile copyFileCompiler
+  match
+    ( "images/*"
+        .||. "robots.txt"
+    )
+    $ do
+      route idRoute
+      compile copyFileCompiler
 
   match "css/*" $ do
     route idRoute
